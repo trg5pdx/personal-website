75b5f931c6c1cdd47c57ee8632fe1c6980852e09
Thomas <trg5@pdx.edu>
2021-11-29 11:56:27 -0800

    Added font awesome for icons later, and fixed animation issues in projects section

--- js/animations.js
+++ js/animations.js
@@ -1,21 +1,22 @@
-let project_toggle = (parent, child) => {
-  let selectors = parent + ' + ' + child;
-  $(selectors).slideToggle();
-  $(parent).add(child).toggleClass('project-header-open', 500);
+let project_toggle = (parent, child1, child2) => {
+  let content = parent + ' > ' + child1;
+  let project_button = parent + ' > ' + child2;
+  $(content).slideToggle();
+  $(project_button).toggleClass('project-header-open');
 };
 
 $(document).ready(function () {
   $('.project').hide();
 
   $('.xv6').on('click', function () {
-    project_toggle('#project', '.xv6');
+    project_toggle('.xv6', '.project', '.project-header');
   });
 
   $('.website').on('click', function () {
-    project_toggle('#project', '.website');
+    project_toggle('.website', '.project', '.project-header');
   });
 
   $('.chocan').on('click', function () {
-    project_toggle('#project', '.chocan');
+    project_toggle('.chocan', '.project', '.project-header');
   });
 });
